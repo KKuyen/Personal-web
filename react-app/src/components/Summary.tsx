467de2c6255b6247a9f2b20b2b836996588ce996
redRUM <22521225@gm.uit.edu.com>
2024-07-18 20:46:36 +0700

    back

--- react-app/src/components/Summary.tsx
+++ react-app/src/components/Summary.tsx
@@ -6,23 +6,51 @@
     const changeBackground = () => {
       const element = document.querySelector(`.${styles.anhsang21}`) as HTMLElement;
       const sunElement = document.querySelector(`.${styles.theSun}`) as HTMLElement;
+      const back =document.querySelector(`.${styles.backButton}`) as HTMLElement;
       if (element) {
        
         if (isFirstColor) {
           // Đặt màu nền sang màu thứ hai
         
-          element.animate([
-            { background: 'linear-gradient(90deg, rgba(255, 137, 0, 0.3) 0%, rgba(34, 22, 10, 0.2) 47.16%, rgba(22, 21, 21, 0.2) 84.67%)' },
-            { background: 'linear-gradient(90deg, rgba(255, 137, 0, 0.4) 0%, rgba(34, 22, 10, 0.2) 63.67%, rgba(22, 21, 21, 0.2) 84.67%)' }
-          ], {
-            duration: 500, // 0.5 giây
-            fill: 'forwards' // Giữ trạng thái cuối cùng của animation
-          });
+          
           // Kích hoạt animation cho sunElement
           if (sunElement) {
             sunElement.animate([
               { transform: 'translateX(-100%)', height: '90vh', width: '90vh' },
-              { transform: 'translateX(-140%)', height: '35vh', width: '35vh' }
+              { transform: 'translateX(-120%)', height: '35vh', width: '35vh' }
+            ], {
+              duration: 500, // 0.5 giây
+              fill: 'forwards' // Giữ trạng thái cuối cùng của animation
+            });
+          }
+          back.animate([
+            { transform: 'translateX(-200%)'  },
+            { transform: 'translateX(0%)' }
+          ], {
+            duration: 500, // 0.5 giây
+            fill: 'forwards' // Giữ trạng thái cuối cùng của animation
+          });
+          setIsFirstColor(!isFirstColor);
+        } 
+        // Đảo trạng thái màu nền
+    
+      }
+    };
+    const changeBackgroundDefault = () => {
+      const element = document.querySelector(`.${styles.anhsang21}`) as HTMLElement;
+      const sunElement = document.querySelector(`.${styles.theSun}`) as HTMLElement;
+      if (element) {
+       
+        if (!isFirstColor) {
+          // Đặt màu nền sang màu thứ hai
+        
+          
+          // Kích hoạt animation cho sunElement
+          if (sunElement) {
+            sunElement.animate([
+             
+              { transform: 'translateX(-120%)', height: '35vh', width: '35vh' },
+              { transform: 'translateX(-100%)', height: '90vh', width: '90vh' }
             ], {
               duration: 500, // 0.5 giây
               fill: 'forwards' // Giữ trạng thái cuối cùng của animation
@@ -38,11 +66,22 @@
     return (
       <>
         <div className={styles.function2Container}>
+        <div className={styles.rectangle}>
+
+          <div className={styles.backButton} onClick={changeBackgroundDefault}>
+            <img className={styles.backImg} src="/arrow.png" alt="backButton" />
+            <p className={styles.backTxt}>Back</p>
+          
+          </div>
+         
+        </div>
           <div className={styles.anhsang21}>
+       
 
             <div onClick={changeBackground} className={styles.theSun}>
 
             </div>
+           
         
       
           </div>
